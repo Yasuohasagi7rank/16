68cd18d08a49223b8bdf2ee36d119b5d33f94733
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:08:27 +0300

    Add files via upload

---
+++ 6.31/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _6._31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b = 0;
+            double c = 0;
+            double x = 0;
+
+            Console.WriteLine("Первое число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+      
+            while (a > 0)
+            {
+                x = a % 10;
+                if (x > b) { b = x; c = 0; }
+                if (x == b) c++;
+                a /= 10;
+            }
+            Console.WriteLine(c);
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
